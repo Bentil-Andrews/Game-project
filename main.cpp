d740a27b0df097a12277b4c064cd7abf7f26015a
Bentil <berngazy59@gmail.com>
2026-02-11 21:36:23 +0000

    Exercise A - Temperature Converter

--- main.cpp
+++ main.cpp
@@ -1,42 +1,25 @@
 #include <iostream>
 using namespace std;
 
-// Function to convert Celsius to Fahrenheit
-double celsiusToFahrenheit(double c) {
-    return (c * 9/5) + 32;
-}
-
-// Function to convert Fahrenheit to Celsius
-double fahrenheitToCelsius(double f) {
-    return (f - 32) * 5/9;
-}
-
 int main() {
-    int choice;
-    double temperature;
+    double celsius, fahrenheit;
 
-    cout << "===== Temperature Converter =====" << endl;
-    cout << "1. Celsius to Fahrenheit" << endl;
-    cout << "2. Fahrenheit to Celsius" << endl;
-    cout << "Choose option: ";
-    cin >> choice;
+    // Ask user for input
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;
 
-    if (choice == 1) {
-        cout << "Enter temperature in Celsius: ";
-        cin >> temperature;
+    // Convert to Fahrenheit
+    fahrenheit = (celsius * 1.8) + 32;
 
-        double result = celsiusToFahrenheit(temperature);
-        cout << "Temperature in Fahrenheit: " << result << endl;
+    // Output result
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    // Bonus conditions
+    if (celsius < 0) {
+        cout << "Freezing!" << endl;
     }
-    else if (choice == 2) {
-        cout << "Enter temperature in Fahrenheit: ";
-        cin >> temperature;
-
-        double result = fahrenheitToCelsius(temperature);
-        cout << "Temperature in Celsius: " << result << endl;
-    }
-    else {
-        cout << "Invalid choice!" << endl;
+    else if (celsius > 30) {
+        cout << "Hot!" << endl;
     }
 
     return 0;
